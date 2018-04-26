76dc96e725f7bfd8e14d83bc17b8ef2e7a3d9c57
WhiteWingedSoul <cooro1994@gmail.com>
2018-04-26 18:10:56 +0900

    change to multi thread

--- main.py
+++ main.py
@@ -3,8 +3,36 @@ import requests
 from twitter.twitter_wrapper import TwitterApi
 from discord.discord_wrapper import DiscordApi
 
-def handle_tweet(tweet):
-	discord.send_discord_message(tweet.text)
+class ACTask:
+
+	account_id = os.environ.get('TWITTER_ACCOUNT_ID')
+	web_hook = os.environ.get('DISCORD_WEBHOOK')
+
+	def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+		self.twitter = TwitterApi(consumer_key, consumer_secret, access_token, access_secret)
+		self.discord = DiscordApi(web_hook)
+
+	def handle_tweet(self, tweet):
+		self.discord.send_discord_message(tweet.text)
+
+	def execute(self):
+		self.twitter.on_status_change(self.account_id, self.handle_tweet)
+
+
+class BCTask:
+
+	account_id = os.environ.get('BCJP_ACCOUNT_ID')
+	web_hook = os.environ.get('BATTLECAT_WEBHOOK')
+
+	def __init__(self, consumer_key, consumer_secret, access_token, access_secret):
+		self.twitter = TwitterApi(consumer_key, consumer_secret, access_token, access_secret)
+		self.discord = DiscordApi(web_hook)
+
+	def handle_tweet(self, tweet):
+		self.discord.send_discord_message(tweet.text)
+
+	def execute(self):
+		self.twitter.on_status_change(self.account_id, self.handle_tweet)
 
 
 consumer_key = os.environ.get('CONSUMER_KEY')
@@ -12,11 +40,4 @@ consumer_secret = os.environ.get('CONSUMER_SECRET')
 access_token = os.environ.get('ACCESS_TOKEN')
 access_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
-account_id = os.environ.get('TWITTER_ACCOUNT_ID')
-web_hook = os.environ.get('DISCORD_WEBHOOK')
-
-twitter = TwitterApi(consumer_key, consumer_secret, access_token, access_secret)
-discord = DiscordApi(web_hook)
-
-twitter.on_status_change(account_id, handle_tweet)
-
+#TODO ThreadPoolExecutor
